10137a33bec78b6209459a4d9ae0e724bdf5e7a9
Dakota Doolaege <dakotadoolaege@gmail.com>
2024-01-05 13:36:26 -0700

    Add process for displaying video stream when streaming is active

--- tello.py
+++ tello.py
@@ -1,21 +1,41 @@
+#!/usr/bin/env python3
 """
 Sends commands to the Tello
 """
 
 import socket
 import multiprocessing
+import cv2
 
 HOST = "192.168.10.1"
 PORT = 8889
+VIDEO_PORT = 11111
 
 def read_response(sock):
     """
     Handles command responses from the Tello drone.
     """
+    print("Started response handler")
     while True:
         data, sender = sock.recvfrom(1024)
         data = data.decode(encoding="utf-8")
-        print(f"Response: {data}")
+        print(f"{sender}: {data}")
+
+def video_stream():
+    """
+    Handles incoming video packets from the drone
+    """
+    print("Started video stream handler")
+    source = f"udp://{HOST}:{VIDEO_PORT}"
+    capture = cv2.VideoCapture(source, cv2.CAP_FFMPEG)
+    if not capture.isOpened():
+        capture.open(source)
+
+    while True:
+        retval, frame = capture.read()
+        if retval:
+            cv2.imshow("frame", frame)
+        cv2.waitKey(1)
 
 def main():
     """
@@ -30,6 +50,7 @@ def main():
 
     command = ""
     sock.sendto("command".encode(encoding="utf-8"), (HOST, PORT))
+    print("Tello control activated. Awaiting your command")
     while True:
         try:
             command = input("")
@@ -37,13 +58,19 @@ def main():
             print(f"Sent: {command}")
             if command == "land":
                 break
+            if command == "streamon":
+                stream_process = multiprocessing.Process(target=video_stream)
+                stream_process.start()
+            if command == "streamoff":
+                stream_process.terminate()
+
         except KeyboardInterrupt:
             sock.close()
             print("Landing...")
             command = "land"
     response_process.terminate()
-    print("Thanks for flying TelloAir, Goodbye.")
+    stream_process.terminate()
+    print("Autonomous landing sequence engaged, Goodbye.")
 
 
 main()
-
